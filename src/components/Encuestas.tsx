dab2b36d11601744264634da39303b0417d3f7dc
Jose <ingborreroj@gmail.com>
2026-06-17 14:52:32 -0400

    Ecuestas Actualizadas

--- src/components/Encuestas.tsx
+++ src/components/Encuestas.tsx
@@ -82,7 +82,7 @@ const Encuestas: React.FC = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50">
+    <div className="fixed bottom-6 right-6 z-[70]">
       {!isOpen && (
         <button
           onClick={() => setIsOpen(true)}
